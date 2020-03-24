d37516f256032bb48b58790b16cd1ce82dc5a0c0
Maxx Tessmer <mhtessmer@gmail.com>
2020-03-23 18:26:13 -0700

    Initial fbs build for windows: working

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SDMCheck',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Maxx Tessmer',
+    author_email='mhtessmer@gmail.com',
+    description='A python package for checking site directed mutagenisis sequences.'
+)
